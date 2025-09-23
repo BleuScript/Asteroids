98e88aeae8a962df640836ecf13817a5db39ef83
Adriel Ortiz <adriel.ortiz.work@gmail.com>
2025-09-23 15:56:14 -0400

    added clock, setting refresh rate to 60 fps

--- main.py
+++ main.py
@@ -6,16 +6,19 @@ from constants import SCREEN_WIDTH
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    nam = 1
-    while nam > 0:
+    clock = pygame.time.Clock()
+    dt = 0
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60)/1000
     print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {constants.SCREEN_WIDTH}") # type: ignore
+    print(f"Screen height: {constants.SCREEN_HEIGHT}") # type: ignore
 
 
 if __name__ == "__main__":
