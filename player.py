9237a47451b9dedf680cb19e8ca0cef14ad3eac3
Adriel Ortiz <adriel.ortiz.work@gmail.com>
2025-09-26 17:45:14 -0400

    just to sync everything up

--- player.py
+++ player.py
@@ -6,14 +6,14 @@ class Player(circleshape.CircleShape): # type: ignore
         self.rotation = 0 # type: ignore
 
     def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        forward = pygame.Vector2(0, 1).rotate(self.rotation) # type: ignore
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5 # type: ignore
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
     
     def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        pygame.draw.polygon(screen, "white", self.triangle(), 2) # type: ignore
 
     self.rotation = 0 # type: ignore
