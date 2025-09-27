209f907d632efd25f6414c66a7f092b8a4c19215
Adriel Ortiz <adriel.ortiz.work@gmail.com>
2025-09-26 20:02:07 -0400

    this is painful

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame # type: ignore
+import pygame # pyright: ignore[reportMissingImports]
 from constants import SCREEN_HEIGHT
 from constants import SCREEN_WIDTH
 import player
@@ -6,26 +6,28 @@ import player
 
 def main():
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    c_player = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
-    c_player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
     while True:
-        player.draw(screen)
+        screen.fill("black")
+        c_player.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
         pygame.display.flip()
-        clock.tick(60)
-        dt = clock.tick(60)/1000
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}") # type: ignore
-    print(f"Screen height: {constants.SCREEN_HEIGHT}") # type: ignore
-
-    
+        hz = int(clock.tick(60))
+        dt = hz/1000
+        
 
 
-if __name__ == "__main__":
-    main()
+
+
+    if __name__ == "__main__":
+        main()
--- player.py
+++ player.py
@@ -1,19 +1,21 @@
 import circleshape
+import pygame # pyright: ignore[reportMissingImports]
+from constants import PLAYER_RADIUS
 
-class Player(circleshape.CircleShape): # type: ignore
+class Player(circleshape.CircleShape):
     def __init__(self, x, y):
-        super().__init__(self.PLAYER_RADIUS)
-        self.rotation = 0 # type: ignore
+        super().__init__(0, 0, self.PLAYER_RADIUS)
+        self.position[0] = 0
+        self.position[1] = 0
+        self.rotation = 0
 
     def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation) # type: ignore
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5 # type: ignore
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
     
     def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2) # type: ignore
-
-    self.rotation = 0 # type: ignore
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
