40ca0c0afe01d14047d16ae5e3c7f2b34c04327d
BleuScript <adriel.ortiz93@gmail.com>
2025-09-30 02:50:45 -0400

    onto asteroids section!

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import player
 
 def main():
     pygame.init()
-
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    player.Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     c_player = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
@@ -14,19 +16,18 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    
     while True:
+        updatable.update(dt)
         screen.fill("black")
-        c_player.draw(screen)
+        for thing in drawable:
+            thing.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.display.flip() 
         dt = clock.tick(60)/1000
         
-
-
-
 
 if __name__ == "__main__":
     main()
