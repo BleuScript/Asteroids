27cd6c7d0abda7033b30fc4cc725573382931cbe
Adriel Ortiz <adriel.ortiz.work@gmail.com>
2025-10-07 00:36:46 -0400

    updating for syncing across devices

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ import pygame # pyright: ignore[reportMissingImports]
 from constants import SCREEN_HEIGHT
 from constants import SCREEN_WIDTH
 import player
+import asteroidfield
+import asteroid
 
 
 def main():
     pygame.init()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     player.Player.containers = (updatable, drawable)
+    asteroid.Asteroid.containers = (asteroids, updatable, drawable)
+    asteroidfield.AsteroidField.containers = (updatable,)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    asteroid_field = asteroidfield.AsteroidField()
     clock = pygame.time.Clock()
     c_player = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
